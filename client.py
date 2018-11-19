02b54ec302e2dd32bc9104063a66093638855217
Jorge De Pablo Martinez <jdepablo@alpha08.aulas.gsyc.urjc.es>
2018-11-19 15:19:10 +0100

    CLIENTE FUNCIONAL PEP 8 FALTA

--- client.py
+++ client.py
@@ -30,7 +30,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
 
-    print('Recibido -- ', data.decode('utf-8'))
+    response = data.decode('utf-8')
+    if response.split()[1] == '100':
+        ack ='ACK sip:' + LOGIN + '@' + SERVER + ' SIP/2.0\r\n'
+        my_socket.send(bytes(ack, 'utf-8') + b'\r\n')
+    else:
+        print(response)
     print('Terminando socket...')
 
 print('Fin.')
