be8799206e49d33b832481946c1e39908a981fba
Jorge De Pablo Martinez <jdepablo@alpha08.aulas.gsyc.urjc.es>
2018-11-19 18:45:58 +0100

    solucionar error sip::: y pasar pep8

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 import os
+import string
 
 try:
     SERVER = sys.argv[1]
@@ -21,6 +22,20 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
 
+    def check_request(self, mess):
+        body = mess.split()[1]
+        version = mess.split()[2]
+        self.correct = True
+        if len(mess.split()) != 3:
+            self.correct = False
+        if version != 'SIP/2.0':
+            self.correct = False
+        if body.find('@') == -1:
+            self.correct = False
+        if body.split(':')[0] != 'sip':
+            self.correct = False
+        return self.correct
+
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         received_mess = []
@@ -28,25 +43,31 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             received_mess = line.decode('utf-8')
             if index == 0:
             # Leyendo primer string que nos envía el cliente
-                if received_mess.split()[1].split[1] != 'SIP/2.0':  #PREGUNTAR !!!!!
-                    self.wfile.write(b'400 Bad Request\r\n\r\n')
-                else:
-                    print("El cliente nos manda " + received_mess)
+                print("El cliente nos manda " + received_mess)
+                if self.check_request(received_mess):
                     method = received_mess.split()[0]
                     if method == 'INVITE':
                         self.wfile.write(b'SIP/2.0 100 Trying\r\n\r\n' +
                                          b'SIP/2.0 180 Ring\r\n\r\n' +
                                          b'SIP/2.0 200 OK\r\n\r\n')
+                        print('Envio un 100 Trying')
+                        print('Envio un 180 Ring')
+                        print('Envio un 200 OK')
                     elif method == 'BYE':
                         self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+                        print('Envio un 200 OK')
                     elif method == 'ACK':
                         ToRun = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + FICH
-                        print('vamos a ejecutar', ToRun)
-                        os.system(ToRun) #ME PETA PREGUNTAR
+                        print('Vamos a ejecutar', ToRun)
+                        os.system(ToRun)
                     else:
                         self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n\r\n')
+                        print('Envio un 405 Method Not Allowed')
+                else:
+                    self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
+                    print('Envio un 400 Bad Request')
             else:
-                # Si no hay más líneas salimos del bucle infinito
+            # Si no hay más líneas salimos del bucle infinito
                 break
 
 if __name__ == "__main__":
